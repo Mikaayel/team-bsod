d65152eeb5743d7829cddf9ea41b58956b814afd
kyle dempsey <kyle.dempsey@purplebricks.com>
2018-02-15 19:03:28 +0000

    improved tooltip

--- components/Tooltips/index.js
+++ components/Tooltips/index.js
@@ -10,26 +10,59 @@ import {
 
 import Helpers from '../../helpers';
 
-const Tooltip = ({ x }) => {
-    return (
-        <View>
-            <VrButton
-                style={{
-                    backgroundColor: '#777879',
-                    layoutOrigin: [.5, .5, 0],
-                    position: 'absolute',
-                    transform: [
-                        { rotateY: x.rotateY },
-                        { translate: [0, 0, -3] }
-                    ]
-                }}
-                onEnter={() => { console.log('enter') }}
-                onExit={() => { console.log('exit') }}
-            >
-                <Text>{x.text}</Text>
-            </VrButton>
-        </View>
-    )
+class Tooltip extends React.Component {
+    
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            tooltip: false
+        }
+    }
+    
+    render() {
+        return (
+            <View>
+                <VrButton
+                    onEnter={() => { this.setState({tooltip: true}) }}
+                    onExit={() => { this.setState({tooltip: false}) }}
+                    style={{
+                        position: 'absolute',
+                        height: 0.15,
+                        width: 0.15,
+                        layoutOrigin: [.5, .5, 0],
+                        borderRadius: 50,
+                        alignItems: 'center',
+                        borderWidth: 0.01,
+                        borderStyle: 'solid',
+                        borderColor: '#FFFFFF80',
+                        justifyContent: 'center',
+                        transform: [
+                            { rotateY: this.props.x.rotateY },
+                            { translate: [-0.25, 0, -3] }
+                        ]
+                }}>
+                    <Text style={{color: 'white'}}>i</Text>
+                </VrButton>
+                {this.state.tooltip &&
+                    <VrButton
+                        style={{
+                            backgroundColor: 'black',
+                            layoutOrigin: [.5, .5, 0],
+                            position: 'absolute',
+                            padding: 0.1,
+                            transform: [
+                                { rotateY: this.props.x.rotateY },
+                                { translate: [0.25, 0, -3] }
+                            ]
+                        }}
+                    >
+                        <Text>{this.props.x.text}</Text>
+                    </VrButton>
+                }
+            </View>
+        )
+    }
 }
 
-export default Tooltip;
+module.exports = Tooltip;
